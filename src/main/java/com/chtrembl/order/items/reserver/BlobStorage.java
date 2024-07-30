f2b591e0356347abd64ad3aaddf948447d889076
Serhii_Kravchenko <Serhii_Kravchenko@epam.com>
2024-07-31 00:06:13 +0300

    upload file with servicebus

--- src/main/java/com/chtrembl/order/items/reserver/BlobStorage.java
+++ src/main/java/com/chtrembl/order/items/reserver/BlobStorage.java
@@ -7,19 +7,35 @@ import com.azure.storage.blob.BlobServiceClientBuilder;
 
 import java.io.InputStream;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class BlobStorage implements Storage {
+
+    private static final Logger log = LoggerFactory.getLogger(BlobStorage.class);
 
     @Override
     public void save(String name, InputStream inputStream) {
-        String connectionString = System.getenv("STORAGE_CONNECTION_STRING");
-        BlobServiceClient blobServiceClient = new BlobServiceClientBuilder()
-                .connectionString(connectionString)
-                .buildClient();
-        BlobContainerClient blobContainerClient = blobServiceClient.getBlobContainerClient("orders");
-        if (!blobContainerClient.exists()) {
-            blobContainerClient.create();
+        int attempts = 0;
+        while (attempts < 3) {
+            try {
+                String connectionString = System.getenv("STORAGE_CONNECTION_STRING");
+                BlobServiceClient blobServiceClient = new BlobServiceClientBuilder()
+                        .connectionString(connectionString)
+                        .buildClient();
+                BlobContainerClient blobContainerClient = blobServiceClient.getBlobContainerClient("orders");
+                if (!blobContainerClient.exists()) {
+                    blobContainerClient.create();
+                }
+                BlobClient blobClient = blobContainerClient.getBlobClient(name);
+                blobClient.upload(inputStream, true);
+                break;
+            } catch (Exception e) {
+                attempts++;
+                if (attempts == 3) {
+                    throw new RuntimeException("Failed after 3 attempts to upload", e);
+                }
+            }
         }
-        BlobClient blobClient = blobContainerClient.getBlobClient(name);
-        blobClient.upload(inputStream, true);
     }
 }
--- src/main/java/com/chtrembl/order/items/reserver/Function.java
+++ src/main/java/com/chtrembl/order/items/reserver/Function.java
@@ -3,18 +3,12 @@ package com.chtrembl.order.items.reserver;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.microsoft.azure.functions.ExecutionContext;
-import com.microsoft.azure.functions.HttpMethod;
-import com.microsoft.azure.functions.HttpRequestMessage;
-import com.microsoft.azure.functions.HttpResponseMessage;
-import com.microsoft.azure.functions.HttpStatus;
-import com.microsoft.azure.functions.annotation.AuthorizationLevel;
 import com.microsoft.azure.functions.annotation.FunctionName;
-import com.microsoft.azure.functions.annotation.HttpTrigger;
+import com.microsoft.azure.functions.annotation.ServiceBusQueueTrigger;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
-import java.util.Optional;
 
 import static java.util.Objects.nonNull;
 
@@ -24,34 +18,26 @@ import static java.util.Objects.nonNull;
 public class Function {
 
     @FunctionName("upload")
-    public HttpResponseMessage run(
-            @HttpTrigger(
-                    name = "req",
-                    methods = {HttpMethod.POST},
-                    authLevel = AuthorizationLevel.ANONYMOUS)
-            HttpRequestMessage<Optional<Order>> request,
+    public void run(
+            @ServiceBusQueueTrigger(
+                    name = "message",
+                    queueName = "orders",
+                    connection = "SERVICE_BUS_CONNECTION_STRING") String message,
             final ExecutionContext context) {
-        context.getLogger().info("Java HTTP trigger processed a request.");
+        context.getLogger().info("Service Bus Trigger executed");
 
-        // Parse query parameter
-        Order order = request.getBody().orElse(null);
-        if (nonNull(order)) {
+
+        if (nonNull(message)) {
             BlobStorage blobStorage = new BlobStorage();
             ObjectMapper mapper = new ObjectMapper();
-            String json = null;
+            Order order = null;
             try {
-                json = mapper.writeValueAsString(order);
+                order = mapper.readValue(message, Order.class);
             } catch (JsonProcessingException e) {
                 throw new RuntimeException(e);
             }
-            InputStream inputStream = new ByteArrayInputStream(json.getBytes(StandardCharsets.UTF_8));
+            InputStream inputStream = new ByteArrayInputStream(message.getBytes(StandardCharsets.UTF_8));
             blobStorage.save(order.getId() + ".json", inputStream);
-        }
-
-        if (order == null) {
-            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body(order.toString()).build();
-        } else {
-            return request.createResponseBuilder(HttpStatus.OK).body(order).build();
         }
     }
 }
